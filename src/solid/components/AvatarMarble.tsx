35164d02f79b488a2f3b60ddb27ff96c68a9218d
rootasjey <5097282+rootasjey@users.noreply.github.com>
2026-07-19 00:37:19 +0200

    🐛 fix(solid): inject marble filter via innerHTML
    Même problème que sunset : colorInterpolationFilters et floodOpacity
    (camelCase) ne sont pas reconnus par SVG via Solid's JSX. Passage
    à color-interpolation-filters et flood-opacity via innerHTML.

--- src/solid/components/AvatarMarble.tsx
+++ src/solid/components/AvatarMarble.tsx
@@ -19,6 +19,7 @@ export function AvatarMarble(allProps: AvatarMarbleProps & JSX.SvgSVGAttributes<
   const data = createMemo(() => generateMarbleData(resolvedName(), resolvedColors()))
   const displaySize = () => Number(props.size ?? 80)
   const mid = DESIGN_SIZE / 2
+  const filterId = () => `${data().maskId}Filter`
 
   return (
     <svg
@@ -37,26 +38,20 @@ export function AvatarMarble(allProps: AvatarMarbleProps & JSX.SvgSVGAttributes<
       <g mask={`url(#${data().maskId})`}>
         <rect width={DESIGN_SIZE} height={DESIGN_SIZE} fill={data().elements[0].color} />
         <path
-          filter={`url(#${data().maskId}Filter)`}
           d="M32.414 59.35L50.376 70.5H72.5v-71H33.728L26.5 13.381l19.057 27.08L32.414 59.35z"
           fill={data().elements[1].color}
+          filter={`url(#${filterId()})`}
           transform={`translate(${data().elements[1].translateX} ${data().elements[1].translateY}) rotate(${data().elements[1].rotate} ${mid} ${mid}) scale(${data().elements[2].scale})`}
         />
         <path
-          filter={`url(#${data().maskId}Filter)`}
           style={{ mixBlendMode: 'overlay' }}
           d="M22.216 24L0 46.75l14.108 38.129L78 86l-3.081-59.276-22.378 4.005 12.972 20.186-23.35 27.395L22.215 24z"
           fill={data().elements[2].color}
+          filter={`url(#${filterId()})`}
           transform={`translate(${data().elements[2].translateX} ${data().elements[2].translateY}) rotate(${data().elements[2].rotate} ${mid} ${mid}) scale(${data().elements[2].scale})`}
         />
       </g>
-      <defs>
-        <filter id={`${data().maskId}Filter`} filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-          <feGaussianBlur stdDeviation="7" result="effect1_foregroundBlur" />
-        </filter>
-      </defs>
+      <defs innerHTML={`<filter id="${filterId()}" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="7" result="effect1_foregroundBlur"/></filter>`} />
     </svg>
   )
 }
